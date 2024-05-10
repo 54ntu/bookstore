eb016dae170d1e8cbb00f5e40750746e1d926289
54ntu <chaudharysantaram8@gmail.com>
2024-05-10 13:01:35 +0545

    frontend components are created

---
+++ frontend/src/components/addbooks.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const addbooks = () => {
+  return (
+    <div>addbooks</div>
+  )
+}
+
+export default addbooks
---
+++ frontend/src/components/bookdetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const bookdetails = () => {
+  return (
+    <div>bookdetails</div>
+  )
+}
+
+export default bookdetails
