bdc4c9086cae0f6cc1a5c083a90af35b491feb89
54ntu <chaudharysantaram8@gmail.com>
2024-05-12 09:59:50 +0545

    addbook frontend functionality is created

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors')
 const { connectdb } = require('./src/db_config/db');
 const { router } = require('./src/routes/book.routes');
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+app.use(cors());
 
 
 //path for the routes
--- frontend/src/components/Addbook.jsx
+++ frontend/src/components/Addbook.jsx
@@ -1,19 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const Addbook = () => {
-  return (
-    <form style={{display:'flex', flexDirection:'column'}}>
-        Name
-        <input type="text" className="title" name='title'/>
-        Author
-        <input type="text" className="author" name='author' />
-        Genre
-        <input type="text" className="genre" name='genre' />
-        Description
-        <textarea name='description'></textarea>
-        <input type='file' name='image'/>
+  const [formdata,setFormdata] = useState({});
+  const [imagedata,setImagedata] = useState();
 
-    </form>
+  const handleChange =(e)=>{
+    setFormdata({...formdata, [e.target.name]:e.target.value});
+  }
+
+
+  const subitbook=async(e)=>{
+    e.preventDefault();
+    const response=await axios.post('http://127.0.0.1:4000/book/v1/add',{
+      ...formdata,
+      image:imagedata
+    })
+    console.log(response)
+
+
+
+  }
+
+  return (
+    <div className='addbook'>
+      <h2 style={{display:'flex', justifyContent:'center'}}>Add book</h2>
+      <hr></hr>
+      <form style={{display:'flex', flexDirection:'column'}} onSubmit={subitbook} encType='multipart/form-data'>
+          Name
+          <input type="text" className="title" name='title' onChange={handleChange}/>
+          Author
+          <input type="text" className="author" name='author'  onChange={handleChange}/>
+          Genre
+          <input type="text" className="genre" name='genre' onChange={handleChange}/>
+          Description
+          <textarea name='description' onChange={handleChange}></textarea>
+          <input type='file' name='image' onChange={(e)=>setImagedata(e.target.value)}/>
+          <input type='submit' value="submit"/>
+      </form>
+    </div>
   )
 }
 
